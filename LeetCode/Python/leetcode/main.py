7cec06c94069069c4a379bdffa4ceb2f7ad46c85
RichearthLee <214503210@qq.com>
2019-01-14 17:37:58 +0800

    update

--- LeetCode/Python/leetcode/main.py
+++ LeetCode/Python/leetcode/main.py
@@ -1,4 +1,5 @@
-from module1 import Solution ;
+#from module1 import Solution
+from module.module1 import Solution
 
 s = Solution()
 grid = [[0] * 4 for i in range(4)]
--- LeetCode/Python/leetcode/module/module1.py
+++ LeetCode/Python/leetcode/module/module1.py
@@ -1,3 +1,5 @@
+from data_structure import ListNode
+
 class Solution(object):
 
     num = 0
@@ -21,8 +23,50 @@ class Solution(object):
                 else:
                     dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
         return dp[-1][-1]
+    
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = ListNode(0)
+        cur = head
+        flag = 0
+        while(l1 != None or l2 != None or flag == 1):
+            if l1 == None and l2 == None:
+                num = 0
+            elif l2 == None:
+                num = l1.val
+                l1 = l1.next
+            elif l1 == None:
+                num = l2.val
+                l2 = l2.next
+            else:
+                num = l1.val + l2.val
+                l1 = l1.next
+                l2 = l2.next
+            node = ListNode(0)
+            node.val = (num + flag)%10
+            flag = (num + flag)/10
+            cur.next = node
+            cur = cur.next
+        return head.next
+    
+    def addTwoNumbers_1(self, l1, l2):
+        carry = 0
+        root = n = ListNode(0)
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            carry, val = divmod(v1+v2+carry, 10)
+            n.next = ListNode(val)
+            n = n.next
+        return root.next
 
 
-# s = Solution()
-# grid = [[0] * 4 for i in range(4)]
-# s.minPathSum(grid)
