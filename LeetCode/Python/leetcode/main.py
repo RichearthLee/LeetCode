f0355c380972fb2f2f053f52b6898920ead67396
Yukun_Lee <214503210@qq.com>
2019-07-19 11:13:04 +0800

    refactoring

--- LeetCode/Python/leetcode/main.py
+++ LeetCode/Python/leetcode/main.py
@@ -1,5 +1,5 @@
 from module import module1
-from structure.data_structure import ListNode
+from utility.data_structure import ListNode
 
 dp = [[1,2,3,4],[1,2,3,5],[1,2,3,4],[1,2,3,4]]
 #print(module1.Solution().longestPalindrome("ABBB"))
--- LeetCode/Python/leetcode/module/module1.py
+++ LeetCode/Python/leetcode/module/module1.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import numpy as np
 
-from structure.data_structure import ListNode
+from utility.data_structure import ListNode
 
 
 class Solution(object):
