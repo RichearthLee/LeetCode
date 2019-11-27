44b596bdf307203ad550752e06d0b72a7bc8524f
liyukun <liyukun@cn.ibm.com>
2019-11-27 13:40:58 +0800

    update

--- LeetCode/Java/src/solutions/Solution2.java
+++ LeetCode/Java/src/solutions/Solution2.java
@@ -780,7 +780,55 @@ public class Solution2 {
         return res;
     }
 
-    
+    /**
+     * 73. Set Matrix Zeroes
+     * @param matrix
+     */
+    public void setZeroes_v1(int[][] matrix) {
+        int[] row = new int[matrix.length];
+        int[] col = new int[matrix[0].length];
+        for(int i = 0 ; i < matrix.length ; ++i){
+            for(int j = 0 ; j < matrix[0].length ; ++j){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for(int i = 0 ; i < matrix.length ; ++i){
+            for(int j = 0 ; j < matrix[0].length ; ++j){
+                if(row[i] == 1){
+                    matrix[i][j] = 0;
+                }
+                if(col[j] == 1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public void setZeroes(int[][] matrix) {
+        boolean col0 = false;
+        for(int i = 0 ; i < matrix.length ; ++i){
+            if(matrix[i][0]==0) col0 = true;
+            for(int j = 1 ; j < matrix[0].length ; ++j){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for(int i = matrix.length - 1 ; i >=0 ; --i){
+            for(int j = matrix[0].length - 1 ; j > 0; --j){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+            if(col0) matrix[i][0] = 0;
+        }
+
+    }
+
+
 
 
 
